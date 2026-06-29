729b2bb224b741bb36b49ab7966a0ae2d82caf63
gabijoann <gabijoann@hotmail.com>
2026-06-29 00:52:22 -0500

    feat: add initial database migration and application configuration file

---
+++ src/Presupuesto.Infrastructure/Migrations/20260629055035_UpdateCategoriaObligacionesToGastosBasicos.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Presupuesto.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategoriaObligacionesToGastosBasicos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "CategoriasPresupuesto",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Nombre",
+                value: "Gastos Básicos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "CategoriasPresupuesto",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Nombre",
+                value: "Obligaciones");
+        }
+    }
+}
